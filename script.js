d6383ad7cf110d96aa9d2d681e16dc46abf1f193
dootalievas <dootalievas@gmail.com>
2022-01-21 16:20:04 -0600

    final version with styled

--- script.js
+++ script.js
@@ -29,24 +29,73 @@ const showProducts = (alldata) => {
 };
 showProducts(alldata);
 
+///local storage name+price
+
+///////filtering products
+const filterProducts = (arr, id) => {
+  return arr.filter((obj) => obj.category === id || obj.sale === id);
+};
+
+const clickCategory = () => {
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", function (e) {
+      buttons.forEach((button) => button.classList.remove("active"));
+      this.classList.add("active");
+      const dataId = e.target.getAttribute("data-id");
+      if (dataId === "home essentials") {
+        clearUi();
+        const filteredProducts = filterProducts(alldata, dataId);
+        console.log(filterProducts, "filtered");
+        showProducts(filteredProducts);
+        categoryName.textContent = "Home Essentials";
+        categoryText.textContent =
+          "Create scented stories tailored to any and every space. Fragrances that set the mood and inspire scented memories. Let our house inspire your home.";
+      } else if (dataId === "cologne") {
+        clearUi();
+        const filteredProducts = filterProducts(alldata, dataId);
+        showProducts(filteredProducts);
+        categoryName.textContent = "Colognes";
+        categoryText.textContent = `Each of our simple, elegant Colognes is crafted from fine ingredients and designed to be layered with another on the skin to create something truly personal to you.`;
+      } else if (dataId === "bath and body") {
+        clearUi();
+        const filteredProducts = filterProducts(alldata, dataId);
+        showProducts(filteredProducts);
+        categoryName.textContent = "Bath and Body";
+        categoryText.textContent = `Create a new morning ritual, a night of pampering or a moment of escape. From uplifting Body & Hand Wash, to sumptuous Bath Oil, all steeped in a selection of our most delicious scents.`;
+      } else if (dataId === "bestseller") {
+        clearUi();
+        const filteredProducts = filterProducts(alldata, dataId);
+        showProducts(filteredProducts);
+        categoryName.textContent = "Bestellers";
+        categoryText.textContent =
+          "Explore a selection of our most-loved scents. Discover the perfect first fragrance, or a new addition to your Jo Malone London collection.";
+      } else if (dataId === "all") {
+        clearUi();
+        showProducts(alldata);
+        categoryName.textContent = "All Products";
+      }
+    });
+  });
+};
+clickCategory();
 //selecting the buttons
 const addToCartbtns = document.querySelectorAll(".addToCart");
 //add to cart arr
 let cart = [];
-addToCartbtns.forEach((btn) =>
-  btn.addEventListener("click", function (e) {
-    const id = +e.target.getAttribute("data-id");
-    console.log(id);
-    const item = alldata.find((el) => el.id === id);
-    cart.push(item);
-    updateCart();
-  })
-);
+const addToCart = () => {
+  addToCartbtns.forEach((btn) =>
+    btn.addEventListener("click", function (e) {
+      const id = +e.target.getAttribute("data-id");
+      console.log(id);
+      const item = alldata.find((el) => el.id === id);
+      cart.push(item);
+      updateCart();
+      console.log(cart);
+    })
+  );
+};
+addToCart();
 ///updating the cart
-function updateCart() {
-  renderCartItems();
-  renderCartSubtotal();
-}
 
 ///calculate subtotal
 function renderCartSubtotal() {
@@ -56,71 +105,27 @@ function renderCartSubtotal() {
     totalProducts++;
     totalPrice = totalPrice + +item.price;
   });
-  subTotalEl.innerHTML = `Subtotal(${totalProducts}): ${totalPrice}`;
+  subTotalEl.innerHTML = `Subtotal(${totalProducts} items): $ ${totalPrice}`;
 }
 
 //renderCartItems function
 
 function renderCartItems() {
   cart.forEach((item) => {
-    console.log(typeof item.price);
-    cartItemsEl.innerHTML += `<div>
+    const div = document.createElement("div");
+    div.innerHTML = `
     <h3 class="nameOfProduct">${item.productName}</h3>
     <img src="${item.image}" style="width:10%"alt="imageOfProduct" />
-    <p class="priceOfProduct">Price:${item.price}</p>
-    </div>
-    
+    <p class="priceOfProduct">Price:$ ${item.price}</p>
     `;
+    div.className = "cart-card";
+    cartItemsEl.appendChild(div);
   });
 }
-console.log(cart);
-
-///local storage name+price
-
-///////filtering products
-const filterProducts = (arr, id) => {
-  return arr.filter((obj) => obj.category === id || obj.sale === id);
-};
-
-buttons.forEach((btn) => {
-  btn.addEventListener("click", function (e) {
-    buttons.forEach((button) => button.classList.remove("active"));
-    this.classList.add("active");
-    const dataId = e.target.getAttribute("data-id");
-    if (dataId === "home essentials") {
-      clearUi();
-      const filteredProducts = filterProducts(alldata, dataId);
-      showProducts(filteredProducts);
-      categoryName.textContent = "Home Essentials";
-      categoryText.textContent =
-        "Create scented stories tailored to any and every space. Fragrances that set the mood and inspire scented memories. Let our house inspire your home.";
-    } else if (dataId === "cologne") {
-      clearUi();
-      const filteredProducts = filterProducts(alldata, dataId);
-      showProducts(filteredProducts);
-      categoryName.textContent = "Colognes";
-      categoryText.textContent = `Each of our simple, elegant Colognes is crafted from fine ingredients and designed to be layered with another on the skin to create something truly personal to you.`;
-    } else if (dataId === "bath and body") {
-      clearUi();
-      const filteredProducts = filterProducts(alldata, dataId);
-      showProducts(filteredProducts);
-      categoryName.textContent = "Bath and Body";
-      categoryText.textContent = `Create a new morning ritual, a night of pampering or a moment of escape. From uplifting Body & Hand Wash, to sumptuous Bath Oil, all steeped in a selection of our most delicious scents.`;
-    } else if (dataId === "bestseller") {
-      clearUi();
-      const filteredProducts = filterProducts(alldata, dataId);
-      showProducts(filteredProducts);
-      categoryName.textContent = "Bestellers";
-      categoryText.textContent =
-        "Explore a selection of our most-loved scents. Discover the perfect first fragrance, or a new addition to your Jo Malone London collection.";
-    } else if (dataId === "all") {
-      clearUi();
-      showProducts(alldata);
-      categoryName.textContent = "All Products";
-    }
-  });
-});
-
+function updateCart() {
+  renderCartItems();
+  renderCartSubtotal();
+}
 ///clean container function
 const clearUi = () => {
   while (container.firstChild) {
@@ -128,5 +133,3 @@ const clearUi = () => {
   }
 };
 // clearUi();
-
-console.log(alldata);
